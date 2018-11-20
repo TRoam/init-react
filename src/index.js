e8a0c6e22d3ffc6c483494833060538a9a9cd25c
wanfeng.tang <wanfeng.tang@envisioncn.com>
2018-11-20 15:02:30 +0800

    just a stash

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from '~/routes';
 import * as serviceWorker from '~/utils/serviceWorker';
 
+// import style css
+import '~/assets/css/style.scss';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
